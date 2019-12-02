831cf447f9f721bdbae1bb8e5ec16ee36f9fb7dc
1styrGroupB <1styrGroupB@pn.local>
2019-12-02 20:53:06 +0800

    statistic

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,5 +1,5 @@
 <template>
-     <apexchart width="380" type="donut" :options="options" :series="series"></apexchart>  
+     <apexchart width="380" type="donut" :options="municipality" :series="series"></apexchart>  
 </template>
 <script>
 import Vue from 'vue'
@@ -12,8 +12,16 @@ import  {getByLocation} from '../helpers/actions'
 export default {
     data: function() {
     return {
-      options: {},
+      // options: {},
+      municipality:{},
       series: [100, 60, 20, 10, 5,5]
+    }
+  },
+  methods:{
+    retriveLocation(){
+       getByLocation()
+        .then(data => ((this.municipality = data.data), console.log(data.data)))
+        .catch(err => alert(err));
     }
   },
    mounted() {
